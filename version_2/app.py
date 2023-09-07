88baa295a58e139f26fbd13b0208b7364ef970b3
stinkyBootsLLC <stinkyBootsLLC@gmail.com>
2023-09-07 19:44:43 -0400

    added 3 main functions

--- version_2/app.py
+++ version_2/app.py
@@ -5,12 +5,31 @@ I need to define the functions of the app first
 
 Then i have to use templates structure for flask
 """
+import config
+import requests
 
-def get_lat_and_long(zip_code):
-    pass
+def get_lat_and_long(zip_code): 
+
+    response = requests.get(f"https://api.openweathermap.org/geo/1.0/zip?zip={zip_code}&appid={config.api_key}").json()
+
+    return [response["lat"], response["lon"]]
 
 def get_current_forcast(lat, long):
-    pass
+
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&units=imperial&appid={config.api_key}").json()
+
+    return response
+     
 
 def get_five_day_forcast(lat, long):
-    pass
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={long}&units=imperial&appid={config.api_key}").json()
+    return response
+
+cordinates = get_lat_and_long("18062")
+print(cordinates)
+
+current_forcast = get_current_forcast(cordinates[0], cordinates[1])
+print(current_forcast)
+
+five_day_forcast = get_five_day_forcast(cordinates[0], cordinates[1])
+print(five_day_forcast)
